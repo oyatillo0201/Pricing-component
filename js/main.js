58e2c98dd0730be30ba48810a0b56a794d49132b
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-08-07 09:55:02 +0300

    Add site pricing component

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elPaymentPeriodRadio =document.querySelectorAll('[name="payment_period"]');
+
+elPaymentPeriodRadio.forEach( function (elPaymentPeriodRadio) {
+  elPaymentPeriodRadio.addEventListener('change', function (
+    
+  ))
+});
